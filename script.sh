3447fd89a16cc09bbe1958c33ad8bf07d4807ef4
Mohamed-ElBanna8070 <121583542+Mohamed-ElBanna8070@users.noreply.github.com>
2022-12-29 00:27:56 +0200

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "$(cat servers)" | (
+  TCP_TIMEOUT=3
+  while read host port; do
+    (CURPID=$BASHPID;
+    (sleep $TCP_TIMEOUT;kill $CURPID) &
+    exec 3<> /dev/tcp/$host/$port
+    ) 2>/dev/null
+    case $? in
+    0)
+      echo $host $port is open;;
+    1)
+      echo $host $port is closed;;
+    143) # killed by SIGTERM
+       echo $host $port timeouted;;
+     esac
+  done
+  ) 2>/dev/null # avoid bash message "Terminated ..."
